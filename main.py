f6c61dc571816b3934a30a363b2b6a8b86e7e42d
GauravPahwa143 <gauravpahwa2020@gmail.com>
2025-01-24 18:25:04 +0530

    data transformation stage added

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import sys
 from networksecuritysystem.exception.exception import NetworkSecuritySystemException
 from networksecuritysystem.logging.logger import logging
-from networksecuritysystem.entity.config_entity import TrainingPipelineConfig,DataIngestionConfig,DataValidationConfig
+from networksecuritysystem.entity.config_entity import TrainingPipelineConfig,DataIngestionConfig,DataValidationConfig,DataTransformationConfig
 from networksecuritysystem.components.data_ingestion import DataIngestion
 from networksecuritysystem.components.data_validation import DataValidation
+from networksecuritysystem.components.data_transformation import DataTransformation
 
 if __name__=='__main__':
     try:
         trainingpipelineconfig=TrainingPipelineConfig()
+        
         dataingestionconfig=DataIngestionConfig(trainingpipelineconfig)
         data_ingestion=DataIngestion(dataingestionconfig)
         logging.info("Initiate the data ingestion")
@@ -21,6 +23,13 @@ if __name__=='__main__':
         datavalidationartifact=data_validation.initiate_data_validation()
         logging.info("Data Validation Completed")
         print(datavalidationartifact)
-        
+
+        datatransformationconfig=DataTransformationConfig(trainingpipelineconfig)
+        data_transformation=DataTransformation(datavalidationartifact,datatransformationconfig)
+        logging.info("Initiate the data transformation")
+        datatransformationartifact=data_transformation.initiate_data_transformation()
+        logging.info("Data Transformation Completed")
+        print(datatransformationartifact)
+
     except Exception as e:
            raise NetworkSecuritySystemException(e,sys)
